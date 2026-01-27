c270b5912ede435210ae867d07137829427e33f2
Wiku Galindra Wardhana <67788111+wikugw@users.noreply.github.com>
2026-01-27 21:49:56 +0700

    add suspense

--- app/(app-layout)/application/form/page.tsx
+++ app/(app-layout)/application/form/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import ApplicationForm from "./ApplicationForm";
 
 export default function ListApplicationPage() {
   return (
-    <div className="flex flex-col gap-2">
-      <ApplicationForm />
-    </div>
+    <Suspense fallback={<div>Loading form...</div>}>
+      <div className="flex flex-col gap-2">
+        <ApplicationForm />
+      </div>
+    </Suspense>
   );
 }
